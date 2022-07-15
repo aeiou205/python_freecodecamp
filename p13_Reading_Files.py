80e90782c5a2b6b3043bbdb98934d884cf2d0957
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-15 12:09:09 -0500

    Create p13_Reading_Files.py
    Reanding file whit open files

---
+++ p13_Reading_Files.py
@@ -0,0 +1,13 @@
+#Using open() for open files 
+#sintaxis
+ ######handle = open(filename, mode)--- ----->return a one file manipulate on handle
+ #fhan = open ('mbox.txt', 'r') -------------> as observed r is the module our propietes of file
+ 
+ stuff = 'hello\nworld'
+ printf(stuff)
+
+ stuff = 'x\ny'
+ print(stuff)
+ len(stuff)
+
+ # all files og text have a \n
