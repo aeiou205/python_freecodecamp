3f1f2160b05ff90c55ac296ce6e795c19c9b561d
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-19 23:55:02 -0500

    Update p22-sorting-list-of-tuples.py

--- p22-sorting-list-of-tuples.py
+++ p22-sorting-list-of-tuples.py
@@ -5,6 +5,38 @@
 #First we sort the dictionary by the key using the items () method
 #and sorted() function
 
+
+from cv2 import sort
+
+
 d = {'a':10, 'b':1, 'c':221}
 print(d.items())
 print(sorted(d.items()))
+
+
+#Using Sorted()
+
+#we can do this even more directly using the built - in function sorted 
+#that takes a sequence as a parameter and returno a sorted sequence
+
+d = {'a':10, 'b':1 , 'c':22 }
+t = sorted(d. items())
+print(t)
+for k, v in sorted(d . items()):
+     print(k,v)
+
+# sort by values instead of key
+
+#if we could construct a list of tuples of the form(values, key) we could sort
+#by value
+
+#we do this with a for loop that creates a list of tuples
+
+c = {'a':10, 'b':1 , 'c':22 }
+tmp = list()
+for k, v in c.items():
+    tmp.append( ( v,k) )
+
+print (tmp)
+tmp = sorted(tmp, reverse = True)
+print (tmp)
