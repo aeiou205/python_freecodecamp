2b6442d236fec23abaf905ccfc8fae75c182c1eb
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-21 13:11:58 -0500

    Update p22-sorting-list-of-tuples.py
    Tuples sintax
    Immutability
    Comparability
    sorting
    Tuples in assignments
    Sorting Dictionaris by either key of value

--- p22-sorting-list-of-tuples.py
+++ p22-sorting-list-of-tuples.py
@@ -25,7 +25,7 @@ print(t)
 for k, v in sorted(d . items()):
      print(k,v)
 
-# sort by values instead of key
+# sort by values instead of key----------------------
 
 #if we could construct a list of tuples of the form(values, key) we could sort
 #by value
@@ -36,7 +36,34 @@ c = {'a':10, 'b':1 , 'c':22 }
 tmp = list()
 for k, v in c.items():
     tmp.append( ( v,k) )
-
+#(22,c)(10,a)(1,b)
+#    v,k  v,k  v,k
 print (tmp)
 tmp = sorted(tmp, reverse = True)
 print (tmp)
+#result final
+#(22,c)(10,a)(1,b)
+#    v,k  v,k  v,k
+
+#----the top 10 most coomon words--------------------------------------------------------
+fnand = open ('filetext.txt')
+count = dict()
+for line in fnand:
+    words = line.split()
+    for word in words:
+        count[word] = count.get(word,0)+1# expresion idiomatica
+        #aqui hace el histograma
+list = list()
+for key,val in count.items():
+    newtup = (val, key) #(tupla se compone del valores y etiquetas)
+    list.append(newtup)
+list = sorted(list, reverse = True) #--------------sorted ordena los valores
+for val , key in list[:10]:         
+    print(key , val)
+
+#------------Even Shorted Version---------------------------------
+#in this case , we make a list of reversed tuples and then sort it
+
+c = {'a':10, 'b':1 , 'c':22}
+print(sorted ([(v,k) for k,v in c.items()]))
+
