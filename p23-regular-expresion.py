6ded93384ecc5cde56d1d0c9ea6fbac5c6e5a74d
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-22 23:14:48 -0500

    Add files via upload
    Regular expresion of old schooll very important because litle pople undestand

---
+++ p23-regular-expresion.py
@@ -0,0 +1,19 @@
+#regular expresion
+# in computing a regular exoreison, also referred to as "regex" or "regexp" 
+#provides a concise and flexible means for matching string of text, such as
+#particular characters, words , or patterns of characters. A regular expression is 
+#writen in a formal languaje that can be interpreted by a regular expression processor 
+
+
+
+#Undestanding Regular Expresssion 
+#Very powerful and quite crytic
+#Fun Once you understan them
+#Regular expressions are a languaje unto thenselves
+#A languaje of "marker characters"- programming with 
+#it is kind of an "ol school" languaje - compact
+#(lenguaje de vieja escuela muy poderoso y divertido)
+
+
+
+
