9a9087bb9fee603c9cfd93d0d13743a61ee1106a
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-25 21:15:20 -0500

    Add files via upload

---
+++ p25-Regular-Expressions:-Practical-Applications.py
@@ -0,0 +1,14 @@
+#String parsing String--------------------
+#Extracting a host name - using find and string
+#Extracción de un nombre de host - usando find y string
+data = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
+atpos = data.find('@')
+print(atpos)
+
+appos = data.find('',atpos)
+print(appos)
+
+host = data[ atpos+1 : appos]
+print(host)
+
+
