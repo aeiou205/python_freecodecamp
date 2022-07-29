f0130f4242a81e4b2343c4774f526f086dda75a5
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-29 17:55:10 -0500

    Add files via upload

---
+++ p30-Networking_Text_Processing.py
@@ -0,0 +1,26 @@
+#0Representing simple string 
+
+
+
+print(ord('d'))
+print(ord('a'))
+print(ord('n'))
+print(ord('i'))
+print(ord('e'))
+print(ord('l'))
+
+# is reccomended practice for encoding data to be exchanged between systems
+
+#-------------------two kind of string in python -------------------------------------------
+x = '贼德'
+
+print(type(x))
+x= u'贼德'
+
+print(type(x))
+
+
+#.................python string to bytes
+
+
+
