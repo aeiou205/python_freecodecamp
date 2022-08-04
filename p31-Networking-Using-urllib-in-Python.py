8a016f599746fce5cf2cedea13976cf3bd3b5951
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-08-03 23:42:02 -0500

    Update p31-Networking-Using-urllib-in-Python.py

--- p31-Networking-Using-urllib-in-Python.py
+++ p31-Networking-Using-urllib-in-Python.py
@@ -1,13 +1,19 @@
 #Using urlib in python
 #since HTTP is common , we have a lubrary that does all the 
 #socket work for us and makes web pages look like a file
+
 """ existe una libreria que nos deja hacer lo mismo que con e
 el socket , esto para recuperar archivos y hacer una llamada 
-al socket """
+al socket ES ELEGANTAMENTE SIMPLE"""
 
-import urllib.request, urllib.parse, urllib.error
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-for line in fhand:
-    print(line.decode().strip())
+#MANDAR UN URL Y LEER EL CONTENIDO WEB
+import urllib.request, urllib.parse, urllib.error 
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt') #LLAMAMOS A LA URLOPEN Y LE DAMOS LA URL
+#CODIFICA , RECUPERA EN BYTES Y LOS DECODIFICA
+for line in fhand:                                               #
+    print(line.decode().strip())# ES COMO LEER , ABRIR ,IMPRIMIR
+
+#------------------------
+
 
 
