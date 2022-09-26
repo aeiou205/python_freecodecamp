4d2c4b2949ec5fa6ada34e12146738758c26ce19
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-25 20:49:58 -0500

    Update p31-Networking-Using-urllib-in-Python.py

--- p31-Networking-Using-urllib-in-Python.py
+++ p31-Networking-Using-urllib-in-Python.py
@@ -15,5 +15,20 @@ for line in fhand:                                               #
 
 #------------------------
 
+counts = dict()
+for line in fhand:
+    words = line.decode.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
+
+
+fhand = urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+for line in fhand:
+    print(line.decode().strip())
+
+
 
 
