98c9ae86ce76ec0bf278cc7ea6938115fec6a7ca
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-25 22:58:27 -0500

    Add files via upload

---
+++ p34-web_services_XML.py
@@ -0,0 +1,36 @@
+#eXtensible Markup Language
+
+"""
+Primary purpose is to helo information system share structured data
+
+it started as a simplified subset of the Standard Generalized Markup Languaje (SGML)
+designed to be relatively
+human-legible
+
+"""
+
+#XML Terminology
+
+"""
+Tags indicate the beninning and ending of elements
+
+Attributes - keyword/value pairs on the opnening tag of XML
+
+Serialize/ De-Serailize - convert data in one program into a common
+format that can be stored and/or transmited between systems in a progrming languaje- independeent 
+manner
+"""
+
+"""
+Star tag        <person>
+end Tag          <name> Chuck </name>
+Text COntent     <phone type = "intl">
+Text content        +1532432432
+Attribute        </phone>
+                 <email hide = "yes" />
+self closing tag </person>
+
+"""
+#los arboles de xml son estados que tienen una direction
+
+
