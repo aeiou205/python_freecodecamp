135594cf2991764bb9bac4bf06e23482e11ffffb
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-25 21:17:37 -0500

    Add files via upload

---
+++ p33-Using-web-servies.py
@@ -0,0 +1,14 @@
+"""
+With the HTTP Request/Response well undestood and well
+supported, there was a natural move toward exchanging data
+between programs using these protocols
+
+
+we needed to come up with an agreed way to represent data going
+between applications and across networks
+
+there are two commonly used formats: XMLS and JSON
+
+
+"""
+
