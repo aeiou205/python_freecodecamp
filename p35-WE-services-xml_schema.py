2a7185a43b36e86e9bf82757369d4dcb42435bb1
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-25 23:54:19 -0500

    Add files via upload

---
+++ p35-WE-services-xml_schema.py
@@ -0,0 +1,17 @@
+#xml Schema
+
+#Description of the legal format of an XML document
+
+#Expresed in terms of constraints on the structure and content od document
+
+#often udes to specif a "contract" between system - "My system will onlu 
+#
+# accep that conforms to his particular schema
+# 
+# 
+# if a particualr piece of xml meet the specification of the schema 
+# 
+# it is said to 'validate'"
+
+
+    
