3ed829c468a5bcf943f9459386a4300ba4d5c48e
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-27 18:35:38 -0500

    Add files via upload

---
+++ p40-web-services-service-oriented-approach.py
@@ -0,0 +1,13 @@
+#Service Oriented Approach
+
+#Most non-trivial web application use services
+#They use services from other application
+#Credit Card Charge
+#Hotal Reservation Systems
+#Services publish the "rules" application must 
+#Services publish the "rules" applications must follow to make use of the services(API)
+
+#Initially - two systems cooperate and split the problem
+
+#As the data/service becomes useful - multiple applications wnats to use
+#the information /applicatoin
