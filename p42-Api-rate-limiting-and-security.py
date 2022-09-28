f6c544ce7bf4db2ef03b71cdfc5806c5a1623424
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-27 19:51:03 -0500

    Update p42-Api-rate-limiting-and-security.py

--- p42-Api-rate-limiting-and-security.py
+++ p42-Api-rate-limiting-and-security.py
@@ -19,4 +19,12 @@ while True:
     print('Rerieving',url)
     connection = urllib.request.urlopen(url)
     data = connection.read().decode()
-    headers = dict()
+    headers = dict(connection.getheaders())
+    print('Remaining',headers['x-rate-limit-remaining'])
+    js = json.loads(data)
+    print(json.dumps(js, indent=4))
+
+    for u in js['users']:
+        print(u['screen_name'])
+        s = u['status']['text']
+        print('',s[:50])
