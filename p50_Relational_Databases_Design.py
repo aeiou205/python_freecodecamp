bfd88a1418ffe52a3f5d8384085fa760ca62cb7e
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-10-02 20:36:41 -0500

    Add files via upload

---
+++ p50_Relational_Databases_Design.py
@@ -0,0 +1,27 @@
+#Databse Design
+
+"""
+-Database desig is an art form of its own with particular skills and experience
+
+-Our goal is to avoid the really bad mistakes and design clean and easily understood databases
+
+-others may performance tune thingslater
+
+-database design start with a picture
+
+"""
+
+#Building a Data Model
+
+"""
+Drawing a picture of the data objects for our application and then
+figuring out how to represent the object and their relationships
+
+Basic Rule: Dont't put the same string data in twice - use a relationship instead
+
+When there is one thing in the " real world" there should be one copy of 
+that thing in the database
+
+
+
+"""
