1860c6a1102ead5c7f598676c2ff76d92dbfce5b
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-10-02 20:37:00 -0500

    Add files via upload

---
+++ p51-relationships-in-a-relational-database.py
@@ -0,0 +1,21 @@
+#Key Rules
+
+"""
+Never use your logical key as the primary key
+
+Logical Keys can and do change, albeit slowly
+
+Relationships thath are based on matching string fields are less efficient that 
+integers
+
+#Foreign Keys 
+
+A foreing key is when a table has a column thath contains a key which 
+poibt to the primary key of another tables
+
+Whenn all primary keys are inteers , then all foreing keys are integers - this is good - very goog
+
+
+
+"""
+
