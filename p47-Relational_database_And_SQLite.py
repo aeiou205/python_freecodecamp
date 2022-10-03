384ad4b30410b9e7ff28a3895518d4724c05f0e6
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-10-02 20:36:05 -0500

    Add files via upload

---
+++ p47-Relational_database_And_SQLite.py
@@ -0,0 +1,27 @@
+#Relational Databases and sqlite
+
+
+#Database contains many tables
+# Relation (or tables) contains tuples and attributes
+#Turple( or row ) - a set of fields that generally represent an "object" 
+#like a person or a music track
+
+#Attribute(also column or field) - one of possibly many elements of data corresponding
+#to the object represented by the row
+
+#Attribute(also column or field ) - one of possibily many elements of
+#data corresponidn to the object reoresented by the row
+
+
+"""
+Web Aplication w/ Databases
+
+Aplication Developer - Builds the logic for the application . the look and feel of the application - monitor
+the application for problems
+
+Database AAministatrod - Monitor and adjust the database as the program runs in production
+
+often both people participate in the building of the Data model
+
+"""
+
